3fefe70a544d24ee115710076e8f78c6807f2886
Hannah Tucker <hannah.tucker@ticketmaster.co.uk>
2020-10-11 14:30:28 +0100

    Add icons

--- inline-edit/src/App.js
+++ inline-edit/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import './App.css';
+import CheckmarkCircled from './icons/CheckmarkCircled';
+import CrossCircled from './icons/CrossCircled';
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <div>Hello World</div>
+      <CheckmarkCircled />
+      <CrossCircled />
+    </>
+  );
 }
 
 export default App;
---
+++ inline-edit/src/icons/CheckmarkCircled.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const CheckmarkCircled = ({ size = '1.5em' }) => (
+  <svg
+    className="baseSvg"
+    viewBox="0 0 24 24"
+    width={size}
+    height={size}
+    fill="#00e1ae"
+    aria-hidden="true"
+  >
+    <path d="M22 12a10.01 10.01 0 01-20 0 10.01 10.01 0 0120 0zM9.73 14.94l-3.08-3.17a.4.4 0 00-.57 0l-.59.59a.4.4 0 000 .56l3.67 3.76a.8.8 0 001.15 0l8.02-8.08a.4.4 0 000-.56l-.6-.6a.4.4 0 00-.56 0l-7.44 7.5z" />
+  </svg>
+);
+
+export default CheckmarkCircled;
